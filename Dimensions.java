c83b843ccba721951243715c175bbe904a9b92d2
Rolol0ko <ronan.keown613@gmail.com>
2022-12-09 22:18:06 -0500

    Start work on new part

---
+++ Dimensions.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Dimensions{
+    public static void runWindow(){
+        JFrame f = new JFrame();
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setLayout(null);
+        f.setSize(500, 150);
+        f.setVisible(true);;
+    }
+}
